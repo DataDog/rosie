af082ece45f8e579ef982d2254c958e95fa3efa4
Julien Delange <julien@gunnm.org>
2022-09-21 14:01:04 -0600

    add more metrics on error

--- src/main/java/io/codiga/server/ServerErrorController.java
+++ src/main/java/io/codiga/server/ServerErrorController.java
@@ -1,11 +1,15 @@
 package io.codiga.server;
 
 
+import io.codiga.metrics.MetricsInterface;
+import io.codiga.server.services.InjectorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static io.codiga.metrics.MetricsName.METRIC_PAGE_NOT_FOUND;
 
 /**
  * Handle 404 and other pages not being found.
@@ -13,11 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServerErrorController implements ErrorController {
 
+    final InjectorService injectorService;
     private final Logger logger = LoggerFactory.getLogger(ServerErrorController.class);
+    private MetricsInterface metrics;
+
+
+    public ServerErrorController(InjectorService injectorService) {
+        metrics = injectorService.getInjector().getInstance(MetricsInterface.class);
+        this.injectorService = injectorService;
+    }
 
     @RequestMapping("/error")
     public String error() {
+        metrics.incrementMetric(METRIC_PAGE_NOT_FOUND);
         return "oh no!";
+
     }
 
 }
