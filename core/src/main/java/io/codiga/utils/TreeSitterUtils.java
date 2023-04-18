a3d44971609412892b33b4a99b881888977cd1f8
Daniel Strong <daniel.strong@datadoghq.com>
2023-04-17 17:56:54 -0700

    remove unused util

--- core/src/main/java/io/codiga/utils/TreeSitterUtils.java
+++ core/src/main/java/io/codiga/utils/TreeSitterUtils.java
@@ -32,25 +32,7 @@ public class TreeSitterUtils {
         }
         return Optional.empty();
     }
-
-    public static Optional<Node> getRootNode(String code, Language language) {
-        Optional<Long> treeSitterLanguage = languageToTreeSitterLanguage(language);
-        Optional<Node> rootNode = Optional.empty();
-        if (treeSitterLanguage.isEmpty()) {
-            return Optional.empty();
-        }
-
-        try (Parser parser = new Parser()) {
-            parser.setLanguage(treeSitterLanguage.get());
-            try (Tree tree = parser.parseString(code)) {
-                rootNode = Optional.of(tree.getRootNode());
-            } catch (UnsupportedEncodingException e) {
-                logger.info("error when decoding the code");
-            }
-        }
-        return rootNode;
-    }
-
+    
     public static Optional<AstElement> getAstElement(Node node, Language language, TreeSitterParsingContext treeSitterParsingContexts) {
         if (language == Language.PYTHON) {
             return TreeSitterPythonParser.parse(node, treeSitterParsingContexts);
