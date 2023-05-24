707e345d11be191642d8738650613c4ee69660cd
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2023-05-24 16:49:41 +0200

    Fixes

--- server/src/main/java/io/codiga/server/request/GetTreeSitterAstRequest.java
+++ server/src/main/java/io/codiga/server/request/GetTreeSitterAstRequest.java
@@ -1,5 +1,6 @@
 package io.codiga.server.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.codiga.model.Language;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
@@ -8,11 +9,13 @@ import lombok.extern.jackson.Jacksonized;
  * The request structure and utils needed to get a TreeSitter AST
  */
 @Builder
+@Jacksonized
 public class GetTreeSitterAstRequest {
     public Language language;
     public String fileEncoding;
     public String code;
 
+    @JsonIgnore
     public boolean isValid() {
         return !(this.fileEncoding == null ||
                 this.language == null ||
